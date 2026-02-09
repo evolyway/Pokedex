f782b8f77ff4ca58746f1eeb08c770a94c4ab42c
Rignchen <rinchendemort@gmail.com>
2026-02-09 01:20:18 +0100

    feat: create sidebar element

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideBar } from '#components/side-bar';
 
 @Component({
 	selector: 'app-root',
-	imports: [RouterOutlet],
+	imports: [RouterOutlet, SideBar],
 	templateUrl: './app.html',
 	styleUrl: './app.css',
 })
