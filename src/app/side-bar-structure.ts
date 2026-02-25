0b6c646441ff25ac3cdd2e8d7a6a2ae391818196
Rignchen <rinchendemort@gmail.com>
2026-02-25 12:52:32 +0100

    style: format code properly

--- src/app/side-bar-structure.ts
+++ src/app/side-bar-structure.ts
@@ -2,31 +2,36 @@ import type { SidebarTree, SidebarNode } from '#types/sidebarStructure';
 import type Camp from '#types/camp';
 import { Role } from '#types/role';
 
-const groupBy = <T>(array: T[], key: (item: T) => string): Record<string, T[]> =>
-	array.reduce((result, item) => ({
-		...result,
-		[key(item)]: [...(result[key(item)] || []), item],
-	}), {} as Record<string, T[]>);
+const groupBy = <T>(
+	array: T[],
+	key: (item: T) => string,
+): Record<string, T[]> =>
+	array.reduce(
+		(result, item) => ({
+			...result,
+			[key(item)]: [...(result[key(item)] || []), item],
+		}),
+		{} as Record<string, T[]>,
+	);
 
 const getRolesGrouped: (
 	roles: Role[],
 	groupBy: (role: Role) => string,
 ) => SidebarNode = (roles: Role[], key: (role: Role) => string) =>
 	/* { "camp": { "name": "/role/normalizedName" } } */
-	Object.entries(groupBy(roles, key))
-		.reduce(
-			(acc, [camp, roles]) => ({
-				...acc,
-				[camp]: roles!.reduce(
-					(acc, role) => ({
-						...acc,
-						[role.name]: `/role/${role.normalizedName}`,
-					}),
-					{} as Record<string, string>,
-				),
-			}),
-			{} as Record<Camp, Record<string, string>>,
-		);
+	Object.entries(groupBy(roles, key)).reduce(
+		(acc, [camp, roles]) => ({
+			...acc,
+			[camp]: roles!.reduce(
+				(acc, role) => ({
+					...acc,
+					[role.name]: `/role/${role.normalizedName}`,
+				}),
+				{} as Record<string, string>,
+			),
+		}),
+		{} as Record<Camp, Record<string, string>>,
+	);
 
 export const getSidebarStructure: (roles: Role[]) => SidebarTree = (
 	roles: Role[],
