acab6d23b457cfd9b3fb571c3c1cc84d58b2acdd
Rignchen <rinchendemort@gmail.com>
2026-02-25 12:51:34 +0100

    refactor: manualy reimplement Object.groupBy
    Object.groupBy apparently doesn't exist on github action pipeline

--- src/app/side-bar-structure.ts
+++ src/app/side-bar-structure.ts
@@ -2,13 +2,18 @@ import type { SidebarTree, SidebarNode } from '#types/sidebarStructure';
 import type Camp from '#types/camp';
 import { Role } from '#types/role';
 
+const groupBy = <T>(array: T[], key: (item: T) => string): Record<string, T[]> =>
+	array.reduce((result, item) => ({
+		...result,
+		[key(item)]: [...(result[key(item)] || []), item],
+	}), {} as Record<string, T[]>);
+
 const getRolesGrouped: (
 	roles: Role[],
 	groupBy: (role: Role) => string,
-) => SidebarNode = (roles: Role[], groupBy: (role: Role) => string) =>
+) => SidebarNode = (roles: Role[], key: (role: Role) => string) =>
 	/* { "camp": { "name": "/role/normalizedName" } } */
-	Object.entries(Object.groupBy(roles, groupBy))
-		.filter(([, roles]) => roles !== undefined)
+	Object.entries(groupBy(roles, key))
 		.reduce(
 			(acc, [camp, roles]) => ({
 				...acc,
