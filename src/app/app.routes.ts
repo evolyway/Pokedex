3a917531731915196747c516f9d8c34fad6a2f85
Rignchen <rinchendemort@gmail.com>
2026-02-27 23:23:09 +0100

    feat: create role by camp page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from '#pages/home';
 import { RoleDetails } from '#pages/role-details';
+import { CampList } from '#pages/camp-list';
 
 export const routes: Routes = [
 	{ path: '', component: Home },
 	{ path: 'role/:name', component: RoleDetails },
+	{ path: 'camp/:name', component: CampList },
 ];
---
+++ src/app/pages/camp-list/camp-list.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CampList } from './camp-list';
+
+describe('CampList', () => {
+  let component: CampList;
+  let fixture: ComponentFixture<CampList>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CampList]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CampList);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
