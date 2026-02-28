c64377192cc470608db60451413c64e0a728a50c
Rignchen <rinchendemort@gmail.com>
2026-02-28 18:25:20 +0100

    refactor: make function to return ServerRoute with only path + options

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -5,6 +5,12 @@ import { inject } from '@angular/core';
 import Camp from '#types/camp';
 import Aura from '#types/aura';
 
+const withPrerenderParams = (path: string, values: Record<string, string[]>): ServerRoute => ({
+	path,
+	renderMode: RenderMode.Prerender,
+	getPrerenderParams: async () => getAllKeyValueCombinations(values),
+});
+
 export const serverRoutes: ServerRoute[] = [
 	{
 		path: 'role/:name',
@@ -16,26 +22,28 @@ export const serverRoutes: ServerRoute[] = [
 			}));
 		},
 	},
-	{
-		path: 'camp/:name',
-		renderMode: RenderMode.Prerender,
-		getPrerenderParams: async () => {
-			return Object.values(Camp).map((camp) => ({
-				name: camp,
-			}));
-		},
-	},
-	{
-		path: 'aura/:name',
-		renderMode: RenderMode.Prerender,
-		getPrerenderParams: async () => {
-			return Object.values(Aura).map((aura) => ({
-				name: aura,
-			}));
-		},
-	},
+	withPrerenderParams('camp/:name', { name: Object.values(Camp) }),
+	withPrerenderParams('aura/:name', { name: Object.values(Aura) }),
 	{
 		path: '**',
 		renderMode: RenderMode.Prerender,
 	},
 ];
+
+function getAllKeyValueCombinations(values: Record<string, string[]>): Record<string, string>[];
+function getAllKeyValueCombinations(values: [string, string[]][]): [string, string][][];
+function getAllKeyValueCombinations(values: Record<string, string[]> | [string, string[]][]): Record<string, string>[] | [string, string][][] {
+	if (!Array.isArray(values)) return getAllKeyValueCombinations(Object.entries(values as Record<string, string[]>)).map(Object.fromEntries);
+
+	if (values.length === 0) return [[]];
+
+	const [[firstKey, firstValues], ...rest] = values;
+	const combinationsOfRest = getAllKeyValueCombinations(rest);
+
+	return firstValues.flatMap((firstValue) =>
+		combinationsOfRest.map((combinationOfRest) => [
+			[firstKey, firstValue] as [string, string],
+			...combinationOfRest,
+		])
+	);
+}
