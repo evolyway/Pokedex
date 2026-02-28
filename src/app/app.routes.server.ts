90b687d3f775e9dba899246ca0e4edf67b353ea7
Rignchen <rinchendemort@gmail.com>
2026-02-28 18:29:42 +0100

    refactor: move getAllKeyValueCombinations to lib

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,6 +4,7 @@ import { Roles } from '#services/roles';
 import { inject } from '@angular/core';
 import Camp from '#types/camp';
 import Aura from '#types/aura';
+import { getAllKeyValueCombinations } from '#lib/record';
 
 const withPrerenderParams = (path: string, values: Record<string, string[]>): ServerRoute => ({
 	path,
@@ -30,20 +31,3 @@ export const serverRoutes: ServerRoute[] = [
 	},
 ];
 
-function getAllKeyValueCombinations(values: Record<string, string[]>): Record<string, string>[];
-function getAllKeyValueCombinations(values: [string, string[]][]): [string, string][][];
-function getAllKeyValueCombinations(values: Record<string, string[]> | [string, string[]][]): Record<string, string>[] | [string, string][][] {
-	if (!Array.isArray(values)) return getAllKeyValueCombinations(Object.entries(values as Record<string, string[]>)).map(Object.fromEntries);
-
-	if (values.length === 0) return [[]];
-
-	const [[firstKey, firstValues], ...rest] = values;
-	const combinationsOfRest = getAllKeyValueCombinations(rest);
-
-	return firstValues.flatMap((firstValue) =>
-		combinationsOfRest.map((combinationOfRest) => [
-			[firstKey, firstValue] as [string, string],
-			...combinationOfRest,
-		])
-	);
-}
