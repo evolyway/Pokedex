f0370aee629745c2e694c77c37d8d041a6058174
Rignchen <rinchendemort@gmail.com>
2026-02-28 16:45:16 +0100

    build: get required prerender parameters

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,8 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 import { Roles } from '#services/roles';
 import { inject } from '@angular/core';
+import Camp from '#types/camp';
+import Aura from '#types/aura';
 
 export const serverRoutes: ServerRoute[] = [
 	{
@@ -11,6 +13,24 @@ export const serverRoutes: ServerRoute[] = [
 			const roles = inject(Roles);
 			return roles.list.map((role) => ({
 				name: role.normalizedName,
+			}));
+		},
+	},
+	{
+		path: 'camp/:name',
+		renderMode: RenderMode.Prerender,
+		getPrerenderParams: async () => {
+			return Object.values(Camp).map((camp) => ({
+				name: camp,
+			}));
+		},
+	},
+	{
+		path: 'aura/:name',
+		renderMode: RenderMode.Prerender,
+		getPrerenderParams: async () => {
+			return Object.values(Aura).map((aura) => ({
+				name: aura,
 			}));
 		},
 	},
