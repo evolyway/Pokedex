3bb75386077f4758e6ad40fc2f2aeb4360c4a272
Rignchen <rinchendemort@gmail.com>
2026-03-01 22:19:11 +0100

    fix: use harness's component instead of generic one

--- src/app/pages/role-details/role-details.spec.ts
+++ src/app/pages/role-details/role-details.spec.ts
@@ -3,10 +3,13 @@ import { RouterTestingHarness } from '@angular/router/testing';
 
 import { provideRouter } from '@angular/router';
 import { RoleDetails } from './role-details';
+import { Roles } from '#services/roles';
+
+import { Role } from '#types/role';
+import Camp from '#types/camp';
+import Aura from '#types/aura';
 
 describe('RoleDetails', () => {
-	let component: RoleDetails;
-	let fixture: ComponentFixture<RoleDetails>;
 	let harness: RouterTestingHarness;
 
 	beforeEach(async () => {
@@ -14,23 +17,23 @@ describe('RoleDetails', () => {
 			imports: [RoleDetails],
 			providers: [
 				provideRouter([{ path: 'roles/:name', component: RoleDetails }]),
+				{ provide: Roles, useValue: { list: [] }}
 			]
 		}).compileComponents();
 		harness = await RouterTestingHarness.create();
-
-		fixture = TestBed.createComponent(RoleDetails);
-		component = fixture.componentInstance;
-		await fixture.whenStable();
 	});
 
-	it('should create', () => {
-		expect(component).toBeTruthy();
+	it.concurrent('should create', () => {
+		const fixture = TestBed.createComponent(RoleDetails);
+		const componentInstance = fixture.componentInstance;
+		expect(componentInstance).toBeTruthy();
 	});
 
-	it('should show "Role not found" if role does not exist', () => {
-		harness.navigateByUrl('/roles/nonexistent');
-		fixture.detectChanges();
-		const compiled = fixture.nativeElement as HTMLElement;
+	it('should show "Role not found" if role does not exist', async () => {
+		const activeComponent = await harness.navigateByUrl('/roles/nonexistent') as RoleDetails;
+		const compiled = harness.fixture.nativeElement as HTMLElement;
+		expect(activeComponent).toBeInstanceOf(RoleDetails);
+		expect(activeComponent.role()).toBeUndefined();
 		expect(compiled.textContent).toContain('Rôle non trouvé.');
 	});
 });
